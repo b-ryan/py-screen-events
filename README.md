2f010eb079ba7371fbc3f8203730b57929eb7403
Buck Ryan <buck.ryan@gmail.com>
2014-10-20 21:50:43 -0400

    first take at Linux equivalent

--- README.md
+++ README.md
@@ -2,3 +2,18 @@ py-screen-events
 ================
 
 A python library to detect when your MAC screen is turned off.
+
+Relevant links
+==============
+
+Bug report for GetActive (dbus org.gnome.ScreenSaver) changing in trusty:
+
+https://bugs.launchpad.net/unity/+bug/1342152
+
+Python dbus examples (particularly interesting is INTROSPECTIVE_IFACE):
+
+http://en.wikibooks.org/wiki/Python_Programming/Dbus
+
+Docs for org.gnome.ScreenSaver:
+
+https://people.gnome.org/~mccann/gnome-screensaver/docs/gnome-screensaver.html#gs-method-GetActive
--- screen_events.py
+++ screen_events.py
@@ -1,11 +1,18 @@
+#!/usr/bin/env python
 import platform
 import threading
 import time
 
 mac = platform.system() == 'Darwin'
+linux = platform.system() == 'Linux'
 
 if mac:
     import Quartz
+elif linux:
+    import dbus
+    from dbus import glib
+    from dbus.mainloop.glib import DBusGMainLoop
+    import gobject
 else:
     raise NotImplemented()
 
@@ -33,20 +40,42 @@ if mac:
            d.get('kCGSSessionOnConsoleKey', 0) == 1:
             return ON
         return OFF
+elif linux:
+    DBUS_UNLOCKED = 0
+    DBUS_LOCKED = 1
 
-    def event_loop(cbk, sleep_time=1, emit_first=True):
-        state = screen_state()
-        if emit_first:
-            cbk(state)
-        while True:
-            new_state = screen_state()
-            if state != new_state:
-                cbk(new_state)
-                state = new_state
-            time.sleep(sleep_time)
+    gobject.threads_init()
+    glib.init_threads()
+
+    _bus = dbus.SessionBus()
+    _screen_saver = dbus.Interface(_bus.get_object('org.gnome.ScreenSaver',
+                                                   '/org/gnome/ScreenSaver'),
+                                   'org.gnome.ScreenSaver')
+    _session = dbus.Interface(_bus.get_object('com.canonical.Unity',
+                                              '/com/canonical/Unity/Session'),
+                              'com.canonical.Unity.Session')
+
+    # y = dbus.Interface(x, dbus.INTROSPECTABLE_IFACE)
+    # print y
+    # print y.Introspect()
+
+    def screen_state():
+        if _session.IsLocked():
+            return OFF
+        return ON if _screen_saver.GetActive() == DBUS_UNLOCKED else OFF
+
+def event_loop(cbk, sleep_time=1, emit_current=True):
+    state = screen_state()
+    if emit_current:
+        cbk(state)
+    while True:
+        new_state = screen_state()
+        if state != new_state:
+            cbk(new_state)
+            state = new_state
+        time.sleep(sleep_time)
 
 if __name__ == '__main__':
     def cbk(state):
         print state
-
     event_loop(cbk)
